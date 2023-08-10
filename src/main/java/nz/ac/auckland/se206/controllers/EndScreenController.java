b4e50332fbd3114deaa73b50bb07f4cb76fe0548
minghan36 <mhua948@aucklanduni.ac.nz>
2023-08-10 15:52:19 +1200

    feat: Added end screen once game over
    This commit created an end screen for when the player wins.

---
+++ src/main/java/nz/ac/auckland/se206/controllers/EndScreenController.java
@@ -0,0 +1,13 @@
+package nz.ac.auckland.se206.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class EndScreenController {
+    
+    @FXML private Label labelEnd;
+
+    public void initialize(){
+        
+    }
+}
